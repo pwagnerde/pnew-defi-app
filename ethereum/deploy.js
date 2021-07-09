e3d826b90b99c20ee7a33e9d4f89f2f56a0a1c3f
Philipp W <74567206+pwagnerde@users.noreply.github.com>
2021-07-09 17:52:45 +0200

    Integration smart contracts

---
+++ ethereum/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  "wheat skull speed peace alarm stumble clip cash quick melt language mimic",
+  "https://rinkeby.infura.io/v3/a6a06423c1ae4218a1068af9613476e1"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
